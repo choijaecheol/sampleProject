f2d285360b16067b72e76e3a113a8c56977ffd7a
최재철(ChoiJaechul)/Platform기술팀/SK <08265@skcc.com>
2016-06-30 18:37:00 +0900

    no message

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sampleProjectApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sampleProjectApp
+ */
+angular.module('sampleProjectApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
